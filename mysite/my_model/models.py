0280842655ae1fc6a816b280eb7fcb6c3ec0e77c
lp1506947671 <1506946771@qq.com>
2021-02-18 07:40:01 +0800

    [类型]:需求开发 [描述]:ORM生成关联表模型 [提交人]:Jason

--- mysite/my_model/models.py
+++ mysite/my_model/models.py
@@ -12,10 +12,34 @@ publish:char
 """
 
 
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=32)
+    age = models.IntegerField()
+    authorDetail = models.OneToOneField(to="AuthorDetail", to_field='id', on_delete=models.CASCADE)
+
+
+class AuthorDetail(models.Model):
+    id = models.AutoField(primary_key=True)
+    birthday = models.DateField()
+    telephone = models.BigIntegerField()
+    addr = models.CharField(max_length=64)
+
+
+class Publish(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=32)
+    city = models.CharField(max_length=32)
+    email = models.EmailField()
+
+
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=32)
     state = models.BooleanField()
     pub_date = models.DateField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    publish = models.CharField(max_length=32)
+    # 与Publish建立一对多的关系,外键字段建立在多的一方
+    publish = models.ForeignKey(to="Publish", to_field="id", on_delete=models.CASCADE)
+    # 与Author表建立多对多的关系,ManyToManyField可以建在两个模型中的任意一个，自动创建第三张表
+    authors = models.ManyToManyField(to='Author')
