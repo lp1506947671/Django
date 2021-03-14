028d497684bca24d3c6b94fccf822c4de8123f7a
lp1506947671 <1506946771@qq.com>
2021-03-14 17:16:13 +0800

    [类型]:需求开发 [描述]:局部校验和全局校验及错误信息提示 [提交人]:Jason

--- mysite/books_app/views.py
+++ mysite/books_app/views.py
@@ -131,36 +131,48 @@ def paginator(request):
 
 
 from django import forms
+from django.core.exceptions import ValidationError
 
 wid_01 = forms.TextInput(attrs={"class": "form-control"})
+wid_02 = forms.PasswordInput(attrs={"class": "form-control"})
 
 
 class UserForm(forms.Form):
     name = forms.CharField(max_length=32, label="用户名", widget=wid_01)
-    pwd = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}), label="密码")
-    r_pwd = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}), label="确认密码")
+    pwd = forms.CharField(label="密码", widget=wid_02)
+    r_pwd = forms.CharField(label="确认密码", widget=wid_02)
     email = forms.EmailField(label="邮件", widget=wid_01)
     tel = forms.CharField(max_length=32, label="手机号码", widget=wid_01)
 
+    def clean_name(self):
+        """局部钩子"""
+        val = self.cleaned_data.get("name")
+        if val.isdigit():
+            return val
+        else:
+            raise ValidationError("用户名不能是纯数字!")
+
+    def clean(self):
+        """全局钩子"""
+        pwd = self.cleaned_data.get("pwd")
+        r_pwd = self.cleaned_data.get("r_pwd")
+
+        if pwd == r_pwd:
+            return self.cleaned_data
+        else:
+            raise ValidationError('两次密码不一致!')
+
 
 def register(request):
-    """
-    创建request.POST
-    is_valid
-    cleaned_data
-    errors
-    """
     if request.method == "POST":
+        dict1 = {}
         user = UserForm(request.POST)
         if user.is_valid():
             print(user.cleaned_data)
         else:
-            print(user.cleaned_data)
-            print(user.errors)
-            print(user.errors.get("name"))
-            print(type(user.errors.get("name")))
-            print(user.errors.get("name")[0])
-        return HttpResponse('OK')
+            dict1["clean_error"] = user.errors.get("__all__")
+        dict1["user"] = user
+        return render(request, "register.html", dict1)
     user = UserForm()
 
     return render(request, "register.html", {"user": user})
