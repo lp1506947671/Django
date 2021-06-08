f3ee71c91787aeea188217c334dbad035be781e4
lp1506947671 <1506946771@qq.com>
2021-06-08 19:53:42 +0800

    [类型]:需求开发 [描述]:drf中的Request和Response [提交人]:Jason

--- mysite/students/views.py
+++ mysite/students/views.py
@@ -3,6 +3,10 @@ import json
 # Create your views here.
 from django.views import View
 from django.http.response import JsonResponse, HttpResponse
+from rest_framework import status, renderers
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Student
 from .serializers import StudentModelSerializer, StudentSerializer, StudentModel2Serializer
@@ -21,7 +25,6 @@ class Student2ViewSet(View):
         return JsonResponse(serializer.data, safe=False)
 
     def post(self, request):
-        print(request.body)
         data = json.loads(request.body)
         serializer = StudentModel2Serializer(data=data)
         serializer.is_valid(raise_exception=True)
@@ -55,4 +58,31 @@ class StudentView(View):
         serializer.is_valid(raise_exception=True)
         # save中传递的关键字参数在create,和update方法中也可以获取
         serializer.save()
-        return HttpResponse("更新完成")
+        return Response()
+
+
+class Student3ViewSet(APIView):
+
+    def get(self, request):
+        print(f"Request.query_params{request.query_params}")
+        print(f"Request.data{request.data}")
+        print(f"Request.getlist{request.query_params.getlist('a')}")
+        print(f"Request.get{request.query_params.get('a')}")
+        student = Student.objects.all()
+        serializer = StudentModel2Serializer(instance=student, many=True)
+        response = Response(serializer.data, status=status.HTTP_200_OK, )
+        response.set_cookie("username", "abc")
+        response["company"] = "Oldboy"
+        print(f"response.data{response.data}")
+        print(f"response.content{response.content}")
+        print(f"response.status_code{response.status_code}")
+        return response
+
+    def post(self, request):
+        print(f"request.data{request.data}")
+        print(f"Request.query_params{request.query_params}")
+        data = json.loads(request.body)
+        serializer = StudentModel2Serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return HttpResponse("ok")
