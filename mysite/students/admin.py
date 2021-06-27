a7ff083510a0034d29234b4f8e43f809906417a0
lp1506947671 <1506946771@qq.com>
2021-06-27 14:08:50 +0800

    [类型]:需求开发 [描述]:admin调整列表页展示,编辑页面展示,站点信息 [提交人]:Jason

--- mysite/students/admin.py
+++ mysite/students/admin.py
@@ -3,4 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from students.models import Student
 
-admin.site.register(Student)
+admin.site.site_header = '后台管理系统'
+admin.site.site_title = '后台管理系统MIS'
+admin.site.index_title = '欢迎使用后台管理系统'
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_per_page = 2
+    actions_on_bottom = True
+    actions_on_top = False
+    list_display = ['id', "name", "pub_date"]
+    search_fields = ['name']
+    # fields = ['name']
+    fieldsets = (("基本", {"fields": ["name"]}), ('高级', {
+        'fields': ["sex", "age", "class_num", "description"],
+        'classes': ('collapse',)
+    }))
--- mysite/students/models.py
+++ mysite/students/models.py
@@ -4,11 +4,17 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, verbose_name="姓名", help_text="姓名")
-    sex = models.BooleanField(default=True, verbose_name="性别", help_text="性别")
-    age = models.IntegerField(verbose_name="年龄", help_text="年龄")
-    class_num = models.CharField(max_length=5, verbose_name="班级编号", help_text="班级数")
-    description = models.TextField(max_length=1000, verbose_name="个性签名", help_text="描述")
+    name = models.CharField(max_length=100, verbose_name="姓名")
+    sex = models.BooleanField(default=True, verbose_name="性别")
+    age = models.IntegerField(verbose_name="年龄")
+    class_num = models.CharField(max_length=5, verbose_name="班级编号")
+    description = models.TextField(max_length=1000, verbose_name="个性签名")
+
+    def pub_date(self):
+        return float(self.age)
+
+    pub_date.short_description = "年龄"
+    pub_date.admin_order_field = "id"
 
     class Meta:
         # 设置表名
